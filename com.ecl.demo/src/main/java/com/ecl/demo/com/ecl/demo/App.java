1025cfa3a3f42105077940f99c01afa18027f654
athulpanackal09 <athulpanackal09@gmail.com>
2024-02-21 12:06:37 +0530

    Committed

---
+++ com.ecl.demo/src/main/java/com/ecl/demo/com/ecl/demo/App.java
@@ -0,0 +1,13 @@
+package com.ecl.demo.com.ecl.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
